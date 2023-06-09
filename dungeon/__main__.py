48773bb0e0193d78cc6b059d5ab4389a1f5e5af4
Cezary Fafara <cezary.fafara@solvinity.com>
2023-06-09 16:22:09 +0200

    Fix reference bug

---
+++ dungeon/__main__.py
@@ -0,0 +1,4 @@
+from dungeon.generators import generate_dungeon
+
+if __name__ == "__main__":
+    generate_dungeon(10, 10)
--- dungeon/generators.py
+++ dungeon/generators.py
@@ -10,16 +10,20 @@ def generate_room(x: int, y: int) -> Room:
 
 def _get_room_placement_permission(room_table: list[list], x: int, y: int) -> tuple[bool, bool]:
     """Return tuple indicating if (we can place a room, we HAVE TO place a room)"""
-    up: int | None = room_table[y - 1][x] if y != 0 else None
-    left: int | None = room_table[y][x - 1] if x != 0 else None
-    cross_right: int | None = room_table[y + 1][x + 1] if (y != 0 and x != len(room_table[0])) else None
+    up = left = cross_right = None
+    if y != 0:
+        up = room_table[y - 1][x]
+    if x != 0:
+        left = room_table[y][x - 1]
+    if y != 0 and x < len(room_table[0]) - 1:
+        cross_right = room_table[y - 1][x + 1]
 
     return (left is not None or up is not None), (up is not None and cross_right is None)
 
 
 def generate_dungeon(max_size_x: int, max_size_y: int) -> list[Room]:
     """Builds dungeon map"""
-    tmp_table: list[list[Room | None]] = [[None] * max_size_x] * max_size_y
+    tmp_table: list[list[Room | None]] = [[None] * max_size_x for _ in range(max_size_y)]
 
     # Place first room on random first row
     rand_x = random.randint(0, max_size_x)
@@ -36,8 +40,5 @@ def generate_dungeon(max_size_x: int, max_size_y: int) -> list[Room]:
             if can_be_placed and random.choice([True, False]):
                 room = generate_room(x, y)
                 tmp_table[y][x] = room
-            #can I place a room here?
-            #do I have to place a room here? (final call)
-            # result.append(generate_room(x, y))
 
     return [room for room_list in tmp_table for room in room_list if room is not None]

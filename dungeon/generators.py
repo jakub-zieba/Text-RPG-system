67737d2a70cfaf70775e3dc1de9591f518fd9ae5
Jakub Zięba <sleepe7@gmail.com>
2023-06-09 14:21:55 +0200

    Modify generators removing random ids

--- dungeon/generators.py
+++ dungeon/generators.py
@@ -1,9 +1,13 @@
-import random
-
 from dungeon.room import Room
 
 
-def generate_room() -> Room:
-    x = random.randint(0, 50)
-    y = random.randint(0, 50)
+def generate_room(x: int, y:int) -> Room:
+    """Creates a single room instance"""
+    x = x
+    y = y
     return Room(x, y)
+
+def generate_dungeon(max_dungeon_size:int):
+    """Builds dungeon map"""
+    #for i in range(max_dungeon_size):
+        #generate_room(x, y)

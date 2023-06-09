dd4da8c7fb1a2e40ab30788a4858263f2dffe6e0
Jakub Zięba <sleepe7@gmail.com>
2023-06-09 17:20:17 +0200

    Player class skeleton and some small changes

---
+++ dungeon/item.py
@@ -0,0 +1,7 @@
+
+
+class ConsumableHealthPotion:
+    """Health regenarating potion, consumable item,
+    consume to regain HP"""
+    def __init__(self) -> None:
+        self.healing_value = 500
---
+++ player.py
@@ -0,0 +1,33 @@
+import random
+
+
+class Player:
+    """Class representing a player """
+    def __init__(self):
+        # base parameters for player, stamina and mana are not used atm
+        self.health: int = 2000
+        self.min_base_damage: int = 100
+        self.max_base_damage: int = 200
+        # self.stamina: int = 500
+        # self.mana: int = 500
+        # those can be modified by equipment changes
+        self.min_damage = self.min_base_damage
+        self.max_damage = self.max_base_damage
+        self.backpack = dict()
+    def grab_item(self, item):
+        if item in self.backpack:
+            self.backpack[item] += 1
+        else:
+            self.backpack[item] = 1
+
+    def drop_item(self, item):
+        if self.backpack[item] >= 1:
+            self.backpack[item] -= 1
+            print("You dropped:", item.name, "amount left:", self.backpack[item])
+            return item
+        else:
+            print("No such thing in your backpack.")
+            return None
+
+    def attack(self):
+        return random.randint(self.min_damage, self.max_damage)
